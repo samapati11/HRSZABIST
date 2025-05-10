026f69af54f2687096f6a8bb2c0ab397615688ec
samapati11 <bscs2312131@szabist.pk>
2025-05-10 11:42:19 +0000

    create git API

--- HRAPI/index.js
+++ HRAPI/index.js
@@ -20,37 +20,35 @@ app.get('/',async(requestAnimationFrame,res)=>{
     }
 });
 
-app.get('/emp',async(req,res)=>{
-    try{
-        const result=await pool.query('select * from employees');
-        res.json(result.rows);
-    }catch(err){
-        res.status(500).json({error:err.message});
-    }});
 
-    app.get('/empTotal',async(req,res)=>{
-        try{
-            const result=await pool.query('select count(employee_id) from employees');
-            res.json(result.rows);
-        }catch(err){
-            res.status(500).json({error:err.message});
-        }});
 
-        app.get('/countr',async(req,res)=>{
+        app.get('/employee42',async(req,res)=>{
             try{
-                const result=await pool.query('select * from countries');
+                const result=await pool.query('SELECT e.employee_id, e.first_name, e.last_name, jh.start_date, jh.end_date, jh.job_id, jh.department_id FROM employees e JOIN job_history jh ON e.employee_id = jh.employee_id limit 2');
                 res.json(result.rows);
             }catch(err){
                 res.status(500).json({error:err.message});
             }});
 
-            app.get('/region',async(req,res)=>{
-                try{
-                    const result=await pool.query('select count(*) from regions');
-                    res.json(result.rows);
-                }catch(err){
-                    res.status(500).json({error:err.message});
-                }});
+            app.get('/jobRecords50',async(req,res)=>{
+            try{
+                const result=await pool.query('select e.employee_id, e.first_name,e.last_name,jh.start_date, j.job_id,j.job_title,d.department_id,d.department_name,l.location_id,city,postal_code,c.country_id,c.country_name from job_history jh left outer join employees e on jh.employee_id = e.employee_id  left outer join  jobs j on jh.job_id = j.job_id left outer join departments d on jh.department_id = d.department_id inner join locations l on d.location_id = l.location_id  inner join countries c on l.country_id = c.country_id limit 3;');
+                res.json(result.rows);
+            }catch(err){
+                res.status(500).json({error:err.message});
+            }});
+            app.get('/regions51',async(req,res)=>{
+            try{
+                const result=await pool.query('select r.region_id,r.region_name, c.country_id,c.country_name,l.location_id,l.postal_code,l.city from regions r inner join countries c on r.region_id = c.region_id inner join locations l on c.country_id =l.country_id limit 2'
+);
+                res.json(result.rows);
+            }catch(err){
+                res.status(500).json({error:err.message});
+            }});
+
+
+
+            
             
                 
             app.get('/Dept',async(req,res)=>{
